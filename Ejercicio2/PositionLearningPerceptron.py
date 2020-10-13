56340f16ea940c35ed800f1e274b4d9b5a05f153
Rodrigo Garrido <ragj97@gmail.com>
2020-10-13 19:32:04 -0300

    Finished Perceptron learning and testing

--- Ejercicio2/PositionLearningPerceptron.py
+++ Ejercicio2/PositionLearningPerceptron.py
@@ -1,3 +1,5 @@
+from random import uniform
+
 class PositionLearningPerceptron:
     def __init__(self, lr):
         self.w1 = 0.5
@@ -17,3 +19,36 @@ class PositionLearningPerceptron:
         if (self.w1*x+self.w2*y > self.bias):
             return 1
         return 0
+    
+    def iteration(self, x, y, desiredOutput):
+        realOutput = self.compute(x, y)
+        self.learn(x, y, realOutput, desiredOutput)
+
+
+def lineTraining(per, m, n, points):
+    for _ in range(points):
+        x = uniform(-10, 10)
+        y = uniform(n-10, n+10)
+        des = 0 if (y < m*x+n) else 1
+        per.iteration(x, y, des)
+
+def lineTest(per, m, n, points):
+    successes = 0
+    mistakes = 0
+    for _ in range(points):
+        x = uniform(-10, 10)
+        y = uniform(n-10, n+10)
+        res = per.compute(x, y)
+        des = 0 if (y < m*x+n) else 1
+        if res == des:
+            successes += 1
+        else:
+            mistakes += 1
+    return successes, mistakes
+
+p = PositionLearningPerceptron(0.001)
+m = 1
+n = 2
+lineTraining(p, m, n, 10000000)
+print(lineTest(p, m, n, 3000))
+
