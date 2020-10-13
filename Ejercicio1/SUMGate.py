2551bb98186f458683dca9ecb06f4e2b0bbb12c6
Rodrigo Garrido <ragj97@gmail.com>
2020-10-13 18:40:45 -0300

    Finished SUM

--- Ejercicio1/SUMGate.py
+++ Ejercicio1/SUMGate.py
@@ -2,4 +2,13 @@ from NANDPerceptron import NANDPerceptron
 
 class SUMGate:
     def __init__(self):
-        self.Perceptrons = [NANDPerceptron() for _ in range(5)]
+        self.Perceptron = NANDPerceptron()
+
+    def compute(self, in1, in2):
+        aux1 = self.Perceptron.compute(in1, in2)
+        carry = self.Perceptron.compute(aux1, aux1)
+        aux2 = self.Perceptron.compute(in1, aux1)
+        aux3 = self.Perceptron.compute(aux1, in2)
+        sum = self.Perceptron.compute(aux2, aux3)
+        return sum, carry
+        
